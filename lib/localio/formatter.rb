3660323b2cb0c2a174c2c7be81050557d6858f4e
Robin Heinze <robin.m.heinze@gmail.com>
2016-08-19 11:04:27 -0700

    :full_snake_case formatting option properly converts camelCase to snake_case for Android

--- lib/localio/formatter.rb
+++ lib/localio/formatter.rb
@@ -13,6 +13,8 @@ module Formatter
         key.space_to_underscore.strip_tag.camel_case
       when :snake_case
         key.space_to_underscore.strip_tag.downcase
+      when :full_snake_case
+        key.space_to_underscore.split("_").map { |part| part.underscore }.join("_")
       else
         raise ArgumentError, 'Unknown formatting used. Must use :smart, :none, :camel_case or :snake_case'
     end
