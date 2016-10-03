fa269cb884a3fa83eb9f82037a5d3f1ea40afa04
Amy M Johnson <a.johnson@crowdcompass.com>
2016-10-03 16:08:14 -0700

    CCD-44868: remove commented out code

--- lib/localio/segments_list_holder.rb
+++ lib/localio/segments_list_holder.rb
@@ -11,26 +11,13 @@ class SegmentsListHolder
     binding
   end
 
-
-#   def create_nested_hash
-#   result_hash = {}
-#   segments.each do |(key,value)|
-#     key.split('_').reduce(result_hash) do  |seg_hash, val|
-#       seg_hash[val] ||= {}
-#     end
-#     *key, last = key.split('_')
-#     key.inject(result_hash, :fetch)[last] = value
-#   end
-#   @nested_hash = result_hash
-# end
-
   def create_nested_hash
     nested_hash = Hash.new { |h, k| h[k] = Hash.new(&h.default_proc) }
     self.segments.each do |term|
       temp_h = nested_hash
       nested_keys = term.key.split("_")
       nested_keys.each_with_index do |nested_key, index|
-        break unless temp_h[nested_key].is_a? Hash # This ignores the translation segment if the segment is not nestable
+        break unless temp_h[nested_key].is_a? Hash # This skips the translation segment if segment is not nestable
         if index == nested_keys.size - 1
           temp_h[nested_key] = term.translation
         else
