510107267498284c5d772ba266c5dc4e81f6bd53
Marcos Luna R <marcos.luna@vass.es>
2021-04-25 19:27:41 +0200

    Added custom changes to writers and templates In order to satisfy the tree structure of nodes in the json files, homogeneous changes are added for all the data files

--- lib/localio/template_handler.rb
+++ lib/localio/template_handler.rb
@@ -6,11 +6,13 @@ class TemplateHandler
     full_template_path = File.join(File.dirname(File.expand_path(__FILE__)), "templates/#{template_name}")
     input_file = File.open(full_template_path, 'rb')
     template = input_file.read
+   
     input_file.close
     renderer = ERB.new(template)
     output = renderer.result(segments.get_binding)
     output_file = File.new(generated_file_name, 'w')
-    output_file.write(output)
+    output_replace = output.gsub(",}", "}")
+    output_file.write(output_replace)
     output_file.close
 
     destination_path = File.join(target_directory, generated_file_name)
