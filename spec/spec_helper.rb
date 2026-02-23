0c31f450b5af819b4fe864879dbf110ddc58dd61
Nacho Lopez <nacho@nlopez.io>
2026-02-23 21:53:32 +0100

    test: bootstrap RSpec with spec_helper

---
+++ spec/spec_helper.rb
@@ -0,0 +1,19 @@
+require 'tmpdir'
+require 'fileutils'
+
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+
+Dir[File.expand_path('support/**/*.rb', __dir__)].each { |f| require f }
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |c|
+    c.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+  config.mock_with :rspec do |mocks|
+    mocks.verify_partial_doubles = true
+  end
+  config.shared_context_metadata_behavior = :apply_to_host_groups
+  config.filter_run_when_matching :focus
+  config.disable_monkey_patching!
+  config.order = :random
+end
